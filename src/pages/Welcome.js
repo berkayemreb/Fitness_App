4ce23e799896569df1a6e2389282dc5d156ec848
bbogum19 <berkaye2001@hotmail.com>
2023-01-16 10:41:51 +0300

    Packages required for routing and navigation have been installed.

---
+++ src/pages/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Welcome = () => {
+    return (
+        <View>
+            <Text>Welcome Sayfası</Text>
+        </View>
+    )
+}
+
+export default Welcome;
