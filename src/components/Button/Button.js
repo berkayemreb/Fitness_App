2005fd1cb7802bce5c5d5af39be18da3f1bdb05a
bbogum19 <berkaye2001@hotmail.com>
2023-01-16 10:48:54 +0300

    Created Button component.

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TouchableOpacity } from 'react-native';
+
+const Button = () => {
+    return (
+        <TouchableOpacity>
+            <Text></Text>
+        </TouchableOpacity>
+    )
+}
+
+export default Button;
