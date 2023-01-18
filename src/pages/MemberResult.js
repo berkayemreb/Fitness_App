3146b363adada94ce0d1c639739d71e041ad03f6
bbogum19 <berkaye2001@hotmail.com>
2023-01-18 15:54:28 +0300

    The styling of MemberResult page is completed.

--- src/pages/MemberResult.js
+++ src/pages/MemberResult.js
@@ -1,36 +1,62 @@
 import React from "react";
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
 
 const MemberResult = ({ route }) => {
 
     const { user } = route.params;
-
+    
+    console.log(user);
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.label}>Üye Adı: <Text style={styles.text}> {user.userName}</Text></Text>
-            <Text style={styles.label}>Üye Soyadı: <Text style={styles.text}> {user.userSurname}</Text></Text>
-            <Text style={styles.label}>Üye Yaşı: <Text style={styles.text}> {user.userAge}</Text></Text>
-            <Text style={styles.label}>Üye E-postası: <Text style={styles.text}> {user.userMail}</Text></Text>
+            <View style={styles.inner_container}>
+                <Text style={styles.header}>XYZ Fitness Salonu</Text>
+                <Text style={styles.label}>Üye Adı: <Text style={styles.text}> {user.userName}</Text></Text>
+                <Text style={styles.label}>Üye Soyadı: <Text style={styles.text}> {user.userSurname}</Text></Text>
+                <Text style={styles.label}>Üye Yaşı: <Text style={styles.text}> {user.userAge}</Text></Text>
+                <Text style={styles.label}>Üye E-postası: <Text style={styles.text}> {user.userMail}</Text></Text>
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+
     container: {
-        marginVertical: 30,
-        marginHorizontal: 10,
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#d0d0d0',
     },
+
+    inner_container: {
+        marginHorizontal: 10,
+        borderWidth: 1,
+        borderColor: '#757575',
+        borderRadius: 10,
+        padding: 10,
+        backgroundColor: '#e0e0e0',
+    },
+
+    header: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        textDecorationLine: 'underline',
+        color: '#0288d1',
+        marginBottom: 3,
+    },
+
     label: {
         marginBottom: 5,
         fontSize: 17,
         fontWeight: '700',
-        color: 'red',
+        color: '#f44336',
     },
+
     text: {
         fontSize: 15,
         fontWeight: '500',
-        color: 'black',
+        color: '#212121',
     }
 })
 
