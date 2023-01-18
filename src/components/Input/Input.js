a8a5ea3dc6ecdd716479a5667856d82441da0b9d
bbogum19 <berkaye2001@hotmail.com>
2023-01-18 11:53:08 +0300

    Created Input component.

---
+++ src/components/Input/Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { TextInput, View, Text } from 'react-native';
+import styles from './Input.style';
+
+const Input = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.label}>label</Text>
+            <View style={styles.input_container}>
+                <TextInput />
+            </View>
+        </View>
+    )
+}
+
+export default Input;
--- src/pages/MemberSign.js
+++ src/pages/MemberSign.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { SafeAreaView, Text } from 'react-native';
+import Input from '../components/Input';
 
 const MemberSign = () => {
     return (
         <SafeAreaView>
             <Text>MemberSign a hosgeldin</Text>
+            <Input />
         </SafeAreaView>
     )
 }
