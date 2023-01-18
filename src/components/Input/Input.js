bd3783da5adb094f8c366524214ab162634ace35
bbogum19 <berkaye2001@hotmail.com>
2023-01-18 14:10:10 +0300

    The input data is converted into an object.

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,18 +2,19 @@ import React from "react";
 import { TextInput, View, Text } from 'react-native';
 import styles from './Input.style';
 
-const Input = ({ label, placeholder }) => {
+const Input = ({ label, placeholder, onChangeText }) => {
     return (
-            <View style={styles.container}>
-                <Text style={styles.label}>{label}</Text>
-                <View style={styles.input_container}>
-                    <TextInput
-                        placeholder={placeholder}
-                        cursorColor='#8d8d8d'
-                        selectionColor='#c0c0c0'
-                    />
-                </View>
+        <View style={styles.container}>
+            <Text style={styles.label}>{label}</Text>
+            <View style={styles.input_container}>
+                <TextInput
+                    placeholder={placeholder}
+                    cursorColor='#8d8d8d'
+                    selectionColor='#c0c0c0'
+                    onChangeText={onChangeText}
+                />
             </View>
+        </View>
     )
 }
 
--- src/pages/MemberSign.js
+++ src/pages/MemberSign.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, StyleSheet } from 'react-native';
 
 import Input from '../components/Input';
 import Button from '../components/Button';
 
 const MemberSign = () => {
+
+    const [userName, setUserName] = useState('');
+    const [userSurname, setUserSurname] = useState('');
+    const [userAge, setUserAge] = useState('');
+    const [userMail, setUserMail] = useState('');
+
+    const handleSubmit = () => {
+        const user = {
+            userName,
+            userSurname,
+            userAge,
+            userMail,
+        }
+        // userName: userName , userAge: userAge şeklinde teker teker yapmak yerine bu şekilde aynı isimleriyle yeni bir obje oluşturmuş oluyor(ES6 ile gelen kolaylık)
+        console.log(user);
+    }
+
     return (
         <SafeAreaView style={stlyes.container}>
             <Input
                 label='Üye Adı'
                 placeholder='Üyenin ismini giriniz...'
+                onChangeText={text => setUserName(text)}
             />
             <Input
                 label='Üye Soyadı'
                 placeholder='Üyenin soyismini giriniz...'
+                onChangeText={setUserSurname}
+            // onChangeText de state ataması yaparken yukarıda olduğu gibi de atama yapabilirsin , bu şekilde kısaltadabilirsin (Reactın kolaylığı)
             />
             <Input
                 label='Üye Yaşı'
                 placeholder='Üyenin yaşını giriniz...'
+                onChangeText={setUserAge}
             />
             <Input
                 label='Üye E-posta Adresi'
                 placeholder='Üyenin e-posta adresini giriniz...'
+                onChangeText={setUserMail}
             />
-            
-            <Button text='Kaydı tamamla' onPress={null} />
+
+            <Button text='Kaydı tamamla' onPress={handleSubmit} />
         </SafeAreaView>
     )
 }
